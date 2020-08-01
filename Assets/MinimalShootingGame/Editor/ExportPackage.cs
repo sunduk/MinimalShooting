a6bb180ea87c01277ae551c419b8107002396361
lee.seokhyun <lee.seokhyun@gmail.com>
2020-08-01 15:59:50 +0900

    Add something.

---
+++ Assets/MinimalShootingGame/Editor/ExportPackage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class ExportPackage
+{
+    [MenuItem("Export/Export with tags")]
+    public static void ExportAll()
+    {
+        //string[] projectContent = new string[] { "Assets", "ProjectSettings/TagManager.asset", "ProjectSettings/InputManager.asset", "ProjectSettings/ProjectSettings.asset" };
+        string[] projectContent = new string[] { "Assets", "ProjectSettings/TagManager.asset" };
+        AssetDatabase.ExportPackage(projectContent, "MinimalShooting.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
+        Debug.Log("Project Exported");
+    }
+}
